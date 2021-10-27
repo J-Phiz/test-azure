c046b5fc2fa1d1aea0b47a5ea328cd42e16a68d6
J-Phiz <jp.save@gmail.com>
2021-10-27 15:31:11 +0200

    API: return static datas

--- api/tasks-get/index.js
+++ api/tasks-get/index.js
@@ -1,6 +1,24 @@
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
+    const tasks = [
+        {
+            id: 1,
+            label: "🍔 Eat",
+            status: ""
+        },
+        {
+            id: 2,
+            label: "🛏 Sleep",
+            status: ""
+        },
+        {
+            id: 3,
+            label: "</> Code",
+            status: ""
+        }
+    ];
+
     const name = (req.query.name || (req.body && req.body.name));
     const responseMessage = name
         ? "Hello, " + name + ". This HTTP triggered function executed successfully."
@@ -8,6 +26,6 @@ module.exports = async function (context, req) {
 
     context.res = {
         // status: 200, /* Defaults to 200 */
-        body: responseMessage
+        body: tasks
     };
 }
